52a1fe20399bf4d3123b57678b38eb08b3669aed
kyranruntukahu <s22310310@student.unklab.ac.id>
2025-11-24 11:58:54 +0800

    Page Dashboard dan Tugas hari ini

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import Sidebar from './components/Sidebar'
+import Dashboard from './pages/Dashboard'  
+import TodayTasks from './pages/TodayTasks'  
 
 export default function App() {
   return (
@@ -9,17 +11,17 @@ export default function App() {
       <div className="flex-1 ml-52">
         <main className="container mx-auto p-6">
           <Routes>
-            {/* Placeholder routes - Orang 1-3 akan mengisi ini */}
-            <Route path="/" element={<div>Dashboard Placeholder</div>} />
+            <Route path="/" element={<Dashboard />} /> 
+            <Route path="/tasks/today" element={<TodayTasks />} /> 
+        
             <Route path="/tasks" element={<div>Tasks Placeholder</div>} />
             <Route path="/tasks/add" element={<div>Add Task Placeholder</div>} />
             <Route path="/tasks/edit/:id" element={<div>Edit Task Placeholder</div>} />
-            <Route path="/tasks/today" element={<div>Today Tasks Placeholder</div>} />
             <Route path="/tasks/high" element={<div>High Priority Placeholder</div>} />
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </main>
       </div>
-    </div>
-  )
+    </div>
+  )
 }
